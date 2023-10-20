6244177d9c5be2b7ece7e0c24ff7172c96336c0a
SajeedAninda <sajaninda@gmail.com>
2023-10-20 14:36:34 +0600

    Minor Commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,8 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-
-const uri = "mongodb+srv://sajaninda:OHJawuylsfxy94rV@cluster0.ruhvmdy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruhvmdy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -89,7 +89,7 @@ async function run() {
             const user = req.body;
             //   console.log(user);
             const result = await cartCollection.insertOne(user);
-            console.log(result);
+            // console.log(result);
             res.send(result);
         });
         app.get("/cart", async (req, res) => {
